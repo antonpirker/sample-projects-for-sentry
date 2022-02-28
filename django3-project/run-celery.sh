9d073c1e9b2e24b8918fab4e85c41fd202b4a1d0
Anton Pirker <anton.pirker@sentry.io>
2022-02-28 13:04:54 +0100

    fixed run command

--- django3-project/run-celery.sh
+++ django3-project/run-celery.sh
@@ -3,8 +3,16 @@
 # exit on first error
 set -e
 
-redis-server &
-
 cd demosite
 
+# activate virtual environment
+source .venv/bin/activate
+
+# Install (or update) requirements
+pip install -r requirements.txt
+
+# Start Redis server in background
+redis-server &
+
+# Start Celery
 celery --app demosite worker --loglevel INFO
