a767695141132677ff503a6def64ad20e02b05df
Anton Pirker <anton.pirker@sentry.io>
2022-02-28 11:14:20 +0100

    Removed WunderPreview stuff

--- django3-project/demosite/web/views.py
+++ django3-project/demosite/web/views.py
@@ -2,8 +2,7 @@ import os
 
 from django.shortcuts import render
 
+
 def index(request):
-    context = {
-        'is_wunderpreview': os.environ.get('IS_WUNDERPREVIEW'),
-    }
-    return render(request, 'index.html', context)
+    context = {}
+    return render(request, "index.html", context)
