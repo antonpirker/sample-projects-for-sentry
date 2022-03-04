3b191564c4d0d37c4a83f41393f31cbe870bbec5
Anton Pirker <anton.pirker@sentry.io>
2022-03-04 15:54:54 +0100

    made error message random to trick dedupe code

--- chalice-project/helloworld/chalicelib/utils.py
+++ chalice-project/helloworld/chalicelib/utils.py
@@ -1,9 +1,13 @@
+import uuid
+
+
 class BoomException(Exception):
     pass
 
 
 def boom(x):
     if x > 0:
-        raise BoomException()
+        rand = uuid.uuid4()
+        raise BoomException(f"With random id {rand}")
 
     return "Boom not triggered. Set trigger=1 in query parameters"
